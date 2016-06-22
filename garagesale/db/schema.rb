2ef1d3e813d8d808cb0376dffb0e5e4415e92db1
Sanghamithra Vasireddy <svasireddy@unomaha.edu>
2016-06-22 21:26:29 +0000

    Added User Authentication

--- garagesale/db/schema.rb
+++ garagesale/db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160622195527) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
