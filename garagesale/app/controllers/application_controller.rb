e850ae401f2a8ac04c787c171ba4e7cd51db9ecf
Sanghamithra Vasireddy <svasireddy@unomaha.edu>
2016-06-22 21:36:51 +0000

    User authentication bugs fixed

--- garagesale/app/controllers/application_controller.rb
+++ garagesale/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   def require_user
-    redirect_to 'login' unless current_user
+    redirect_to '/login' unless current_user
   end
   protect_from_forgery with: :exception
 end
