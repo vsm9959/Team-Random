0d4724c538ea20e41b66a6935698298229dbadb7
Sanghamithra Vasireddy <svasireddy@unomaha.edu>
2016-08-16 01:22:08 +0000

    users controller modified

--- garagesale/app/controllers/users_controller.rb
+++ garagesale/app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
             if @user.save
                 UserMailer.welcome_email(@user).deliver_now
                 session[:user_auth_token] = @user.auth_token
-             format.html{   redirect_to '/'}
+             format.html{   redirect_to root_url}
             elsif User.exists?(email: @user.email)
                 format.html{ redirect_to login_path
                     flash[:notice] = 'You have already signed up'
